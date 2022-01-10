7e963a3ca3975401141c0fae17d9c289a5684fcd
Daniel Acosta <danielacosta10@aol.com>
2022-01-09 19:32:26 -0800

    JS practice

--- authorization-form/src/App.js
+++ authorization-form/src/App.js
@@ -2,28 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [pic, setPic] = useState('');
 
-  const incrementHandler = () => {
-    setCount(currentCount => currentCount + 1)
+  const handleClick = () => {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let chosenPic;
+
+    if (randomNumber === 1) {
+      chosenPic = 'https://i2-prod.mirror.co.uk/incoming/article22123981.ece/ALTERNATES/s615b/1_Mike-Tyson-versus-Evander-Holyfield.jpg';
+    } else if (randomNumber === 2) {
+      chosenPic = 'https://i1.sndcdn.com/artworks-000585482786-7cozwy-t500x500.jpg';
+    } else {
+      chosenPic = 'https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F210202095047-tom-brady.jpg';
+    }
+
+    setPic(chosenPic);
   };
 
-  const decrementHandler = () => {
-    setCount(currentCount => currentCount - 1)
-  };
 
   return (
-  <div>
-    <h1>{count}</h1>
-    <button onClick={incrementHandler}>Increase</button>
-    <button onClick={decrementHandler}>Decrease</button>
+    <div>
+      <h1>Image 1</h1>
+      <button type='button' onClick={handleClick}>Click Me</button>
+      <button type="button" onClick={()=> setPic('')}>Click Now</button>
+      <img src={pic}></img>
 
-  </div>
+    </div>
   )
-
-
 };
-
 
 export default App;
