a45d66b08424aa6f2408d6001f5ac018367e5aca
Daniel Acosta <danielacosta10@aol.com>
2022-01-11 20:02:54 -0800

    React practice

--- authorization-form/src/App.js
+++ authorization-form/src/App.js
@@ -3,33 +3,43 @@ import './App.css';
 import React, {useState} from 'react';
 
 const App = () => {
-  const [pic, setPic] = useState('');
+  const [calcNum, setCalcNum] = useState(0);
+
+  
 
   const handleClick = () => {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-    let chosenPic;
-
-    if (randomNumber === 1) {
-      chosenPic = 'https://i2-prod.mirror.co.uk/incoming/article22123981.ece/ALTERNATES/s615b/1_Mike-Tyson-versus-Evander-Holyfield.jpg';
-    } else if (randomNumber === 2) {
-      chosenPic = 'https://i1.sndcdn.com/artworks-000585482786-7cozwy-t500x500.jpg';
-    } else {
-      chosenPic = 'https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F210202095047-tom-brady.jpg';
-    }
-
-    setPic(chosenPic);
+    setCalcNum(prevCount => prevCount +1)
+    numArray.push(calcNum)
   };
+  const handleClick2 = () => {
+    setCalcNum(prevCount => prevCount +2)
+    numArray.push(calcNum)
+  };
+  const handleClick3 = () => {
+    setCalcNum(prevCount => prevCount +3)
+    numArray.push(calcNum)
+  };
+  const handleClickClear = () => {
+    setCalcNum(0)
+  };
+ 
+  let numArray = [];
 
 
   return (
     <div>
-      <h1>Image 1</h1>
-      <button type='button' onClick={handleClick}>Click Me</button>
-      <button type="button" onClick={()=> setPic('')}>Click Now</button>
-      <img src={pic}></img>
+      <button type="button" onClick={handleClick}>1</button>
+      <button type="button" onClick={handleClick2}>2</button>
+      <button type="button" onClick={handleClick3}>3</button>
+      <button type="button" onClick={handleClickClear}>Clear</button>
 
+      <h1>xx{numArray}xx</h1>
+      
+      <h1>{calcNum}</h1>
+      
     </div>
   )
 };
+
 
 export default App;
