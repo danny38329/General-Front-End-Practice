c4f207e68df1358e7248f5b5dbac63e26c481cd4
Daniel Acosta <danielacosta10@aol.com>
2022-01-12 18:42:24 -0800

    Calculator app

--- authorization-form/src/App.js
+++ authorization-form/src/App.js
@@ -5,7 +5,19 @@ import React, {useState} from 'react';
 const App = () => {
   const [calcNum, setCalcNum] = useState(0);
 
-  
+  const numButtons = () => {
+    let digits = [];
+
+    for (let i=1; i < 10; i++) {
+      digits.push(
+        <button
+          key={i}>
+            {i}
+          </button>
+      )
+    }
+    return digits;
+  }
 
   const handleClick = () => {
     setCalcNum(prevCount => prevCount +1)
@@ -33,8 +45,8 @@ const App = () => {
       <button type="button" onClick={handleClick3}>3</button>
       <button type="button" onClick={handleClickClear}>Clear</button>
 
-      <h1>xx{numArray}xx</h1>
-      
+<br></br>
+      {numButtons()}
       <h1>{calcNum}</h1>
       
     </div>
