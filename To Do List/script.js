fa55aae099822fa1d71cd13d4f96742771fda527
Daniel Acosta <danielacosta10@aol.com>
2022-01-21 23:05:24 -0800

    Adding remove function

--- To Do List/script.js
+++ To Do List/script.js
@@ -1,9 +1,11 @@
 const addElement = () => {
-    
-    let newItem = document.createElement('div');
-    newItem.innerHTML = "<input type='text' id='newInputBox'><button type='button'>Remove</button>";
+    let count = 0;
+    let x = document.getElementById("task").value;
+
+    let newItem = document.createElement('p');
+    newItem.id = "todo"+count;
+
+    newItem.innerHTML = "<br> "+ x +" <button type='button'>Remove</button>";
     document.getElementById("list").appendChild(newItem);
-    
-
-
 }
+
