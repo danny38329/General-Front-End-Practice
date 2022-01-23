cf71a47494e9feb739b3f47070ec965a30b5322f
Daniel Acosta <danielacosta10@aol.com>
2022-01-22 22:42:22 -0800

    Adding functions

--- To Do List/script.js
+++ To Do List/script.js
@@ -1,3 +1,7 @@
+const removeElement = () => {
+    newItem.remove();
+}
+
 const addElement = () => {
     let count = 0;
     let x = document.getElementById("task").value;
@@ -5,7 +9,14 @@ const addElement = () => {
     let newItem = document.createElement('p');
     newItem.id = "todo"+count;
 
-    newItem.innerHTML = "<br> "+ x +" <button type='button'>Remove</button>";
+
+
+    newItem.innerHTML = "<br> "+ x +" <button type='button' onclick=\"removeElement()\">Remove</button>";
     document.getElementById("list").appendChild(newItem);
+
+
 }
 
+const removeElement = () => {
+    newItem.remove();
+}
