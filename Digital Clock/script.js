aa9abfed63e37bb1b93c40b76cbf1c85bb1c547e
Daniel Acosta <danielacosta10@aol.com>
2022-01-29 22:05:00 -0800

    Updating time

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,4 +1,9 @@
+const updateTime = () => {
+
 var today = new Date();
 var time = today.getHours() + ":" + today.getMinutes();
 
 document.getElementById("time").innerHTML = time;
+}
+
+setInterval("updateTime", 1000);
