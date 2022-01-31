9db6761928367eff3048a42b34960da659700239
Daniel Acosta <danielacosta10@aol.com>
2022-01-30 21:44:59 -0800

    Final javascript

--- Expense Tracker/script.js
+++ Expense Tracker/script.js
@@ -1,11 +1,18 @@
-let expenseInput = document.getElementById("expense").value;
-let costInput = document.getElementById("cost").value;
+
+
 let listDiv = document.getElementById("list");
+let count = 0;
 
 const addExpense = () => {
+    
+    let expenseInput = document.getElementById("expense").value;
+    let costInput = document.getElementById("cost").value;
     let newExpense = document.createElement("p");
+    
+    newExpense.innerHTML = costInput + "  :  " + expenseInput;
+    count += Number(costInput);
     listDiv.appendChild(newExpense);
-    newExpense.innerHTML = "testing";
-
+    document.getElementById("totalspent").innerHTML = Number(count);
 
 }
+
