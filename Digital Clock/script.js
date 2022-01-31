ff46227b39b953d657c765a9f946c04dc4ec6809
Daniel Acosta <danielacosta10@aol.com>
2022-01-30 19:05:21 -0800

    Digital Clock setinterval addition

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,9 +1,9 @@
 const updateTime = () => {
 
 var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
 document.getElementById("time").innerHTML = time;
 }
 
-setInterval("updateTime", 1000);
+setInterval(updateTime, 1000);
