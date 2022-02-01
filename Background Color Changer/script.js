c182a75704c3f3b9591f82226963d89b62e42b23
Daniel Acosta <danielacosta10@aol.com>
2022-01-31 21:28:35 -0800

    Calculator progression

--- Background Color Changer/script.js
+++ Background Color Changer/script.js
@@ -1,3 +1,14 @@
+
+
 const changeColor = () => {
-    document.getElementById("complete").style.backgroundColor = "red";
+    let randomColor;
+    let randNum = Math.floor(Math.random() * 3);
+    if (randNum === 0) {
+        randomColor = "red";
+    } else if (randNum === 1) {
+        randomColor = "blue";
+    } else {
+        randomColor = "yellow";
+    };
+    document.getElementById("complete").style.backgroundColor = randomColor;
 }
---
+++ Calculator Intermediate Project/script.js
@@ -0,0 +1,74 @@
+let firstNumber = "";
+let secondNumber = "";
+
+const buttonOne = () => {
+    if (firstNumber === "") {
+    document.getElementById("demo").innerHTML = firstNumber += '1';
+    } else {
+        document.getElementById("demo").innerHTML = secondNumber += '1';
+    }
+};
+
+const buttonTwo = () => {
+    if (firstNumber === "") {
+        document.getElementById("demo").innerHTML = firstNumber += '2';
+        } else {
+            document.getElementById("demo").innerHTML = secondNumber += '2';
+        }
+};
+
+const buttonThree = () => {
+    if (firstNumber === "") {
+        document.getElementById("demo").innerHTML = firstNumber += '3';
+        } else {
+            document.getElementById("demo").innerHTML = secondNumber += '3';
+        }
+};
+
+const buttonFour = () => {
+    if (firstNumber === "") {
+        document.getElementById("demo").innerHTML = firstNumber += '4';
+        } else {
+            document.getElementById("demo").innerHTML = secondNumber += '4';
+        }
+};
+
+const buttonFive = () => {
+    if (firstNumber === "") {
+        document.getElementById("demo").innerHTML = firstNumber += '5';
+        } else {
+            document.getElementById("demo").innerHTML = secondNumber += '5';
+        }
+};
+
+const buttonSix = () => {
+    if (firstNumber === "") {
+        document.getElementById("demo").innerHTML = firstNumber += '6';
+        } else {
+            document.getElementById("demo").innerHTML = secondNumber += '6';
+        }
+};
+
+const buttonSeven = () => {
+    if (firstNumber === "") {
+        document.getElementById("demo").innerHTML = firstNumber += '7';
+        } else {
+            document.getElementById("demo").innerHTML = secondNumber += '7';
+        }
+};
+
+const buttonEight = () => {
+    if (firstNumber === "") {
+        document.getElementById("demo").innerHTML = firstNumber += '8';
+        } else {
+            document.getElementById("demo").innerHTML = secondNumber += '8';
+        }
+};
+
+const buttonNine = () => {
+    if (firstNumber === "") {
+        document.getElementById("demo").innerHTML = firstNumber += '9';
+        } else {
+            document.getElementById("demo").innerHTML = secondNumber += '9';
+        }
+};
