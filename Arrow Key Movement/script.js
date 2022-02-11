64efeef9e3145ffe16ece34e458fc8c4a68d58cc
Daniel Acosta <danielacosta10@aol.com>
2022-02-10 22:09:29 -0800

    Adding typescript

---
+++ Arrow Key Movement/script.js
@@ -0,0 +1,5 @@
+var boxOne = document.getElementById("one");
+var boxTwo = document.getElementById("two");
+var boxThree = document.getElementById("three");
+var boxFour = document.getElementById("four");
+var currentBox;
---
+++ Arrow Key Movement/script.ts
@@ -0,0 +1,7 @@
+let boxOne = document.getElementById("one");
+let boxTwo= document.getElementById("two");
+let boxThree = document.getElementById("three");
+let boxFour = document.getElementById("four");
+
+let currentBox;
+
