a9a04d886bbe710746e704698ed5ad5104e3a86b
Daniel Acosta <danielacosta10@aol.com>
2022-02-11 20:04:49 -0800

    Adding more javascript

--- Arrow Key Movement/script.ts
+++ Arrow Key Movement/script.ts
@@ -3,5 +3,7 @@ let boxTwo= document.getElementById("two");
 let boxThree = document.getElementById("three");
 let boxFour = document.getElementById("four");
 
+let prevBox;
 let currentBox;
 
+currentBox.style.backgroundColor = "blue";
