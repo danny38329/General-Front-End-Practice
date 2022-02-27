e813342f2f72c1ef84ee36e4cb5fedd55d04c8e1
Daniel Acosta <danielacosta10@aol.com>
2022-02-26 19:00:25 -0800

    React mastery

---
+++ React Practice/Color Changer Map.jsx
@@ -0,0 +1,16 @@
+const colorChanger = () => {
+
+    const [color, setColor] = useState("green");
+    const colorNames = ["blue", "red", "yellow"];
+    let changeBack = {backgroundColor: color}
+    return (
+        <div>
+
+
+
+        </div>
+
+    )
+
+
+}
---
+++ react-practice/src/index.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+function ChangeColor() {
+  const [color, setColor] = useState("white");
+  setColor("red")
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(prevCount => prevCount + 1);
+  }
+
+return (
+  <div>
+    <h1>The count is {count}</h1>
+    <button onClick={increment}>Add</button>
+
+  </div>
+)
+}
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Counter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
