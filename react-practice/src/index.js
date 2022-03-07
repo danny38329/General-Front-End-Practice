3c2b693bc324a26c6075d0c514916fa4eb1ce866
Daniel Acosta <danielacosta10@aol.com>
2022-03-06 22:50:34 -0800

    Adding extrovert/introvert

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -4,135 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function CalculatorButtons() {
-  let buttonArray = [];
+function ExtrovertOrIntrovert() {
+  const [personality, setPersonality] = useState("");
 
-  for (let i = 0; i < 10; i++) {
-    buttonArray.push(
-      <button>{i}</button>
-    )
+  const extrovert = () => {
+    setPersonality("extrovert");
   }
-  return buttonArray;
-}
 
-
-
- function RandomQuote() {
-  const [quote, setQuote] = useState('');
-  let quoteOne = "yes";
-  let quoteTwo = "no";
-  let quoteThree = "maybe";
-
-  const createRandom = () => {
-  let randomNumber = Math.floor(Math.random() * 3);
-  if (randomNumber === 0) {
-    setQuote(quoteOne);
-  } else if (randomNumber === 1) {
-    setQuote(quoteTwo);
-  } else {
-    setQuote(quoteThree);
-  }};
+  const introvert = () => {
+    setPersonality("introvert");
+  }
 
   return (
     <div>
-      <button onClick={createRandom}>Generate</button>
-      <h1>{quote}</h1>
+      <h1>What is your personality?</h1>
+      <button onClick={extrovert}>Extrovert</button>
+      <button onClick={introvert}>Introvert</button>
+      <h1>{personality}</h1>
     </div>
   )
-
-
- }
-
-
-function CoinToss() {
-  const [face, setFace] = useState('none');
-  const heads = 'heads';
-  const tails = 'tails';
-  
-  
-  const genCoin = () => {
-
-  let randomNum = Math.floor(Math.random() * 2);
-
-  if (randomNum === 0) {
-    setFace('heads');
-  } else {
-    setFace('tails');
-  }
-  return face;
-  };
-
-
-  return (<div>
-    <p>
-      <h1>the coin flip is: {face}</h1>
-    </p>
-      <button onClick={genCoin}>coin</button>
-  </div>)
-
-}
-
-function AddDigitalClock() {
- 
-  const [ticker, setTicker] = useState(time);
-  var today = new Date();
-  let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-  const refreshClock = () => {
-    setTicker(time);
-  }
-  
-return <p></p>
-
-}
-
-
-
-
-
-function ChangeColor() {
-  const [color, setColor] = useState("white");
-  const changeRed = () => {
-
-    setColor("red")
-  }
-
-  const changeBlue = () => {
-
-    setColor("blue")
-  }
-
-  document.body.style.background = color;
-
-  return (
-  <div>
-  <button onClick={changeRed}>Change to Red</button>
-  <button onClick={changeBlue}>Change to Blue</button>
-  </div>
-  )
-}
-
-function Counter() {
-  const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount(prevCount => prevCount + 1);
-  }
-
-return (
-  <div>
-    <h1>The count is {count}</h1>
-    <button onClick={increment}>Add</button>
-    <ChangeColor />
-  
-
-  </div>
-)
 }
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <CalculatorButtons />
+    <ExtrovertOrIntrovert />
   </React.StrictMode>,
   document.getElementById('root')
 );
