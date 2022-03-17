9f9dc6aea8efe6659be20939213c142fa8c62a6e
Daniel Acosta <danielacosta10@aol.com>
2022-03-16 22:23:35 -0700

    Practicing fundamentals

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -4,114 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function BudgetCalculator() {
-  
-}
-
-function HandleInput() {
-  const [name, setName] = useState([]);
-
-  const inputEnter = () => {
-    let count = 0;
-    count ++;
-    name.push([count, name])
-  }
-
-  return(
-    <div>
-      <input type="text" onChange={e => {setName(prevCount => prevCount + "xxx" + e.target.value)}} />
-      <button type="button" onClick={inputEnter}>Add</button>
-      <h1>{name}</h1>
-    </div>
-  )
-
-}
-
-
- 
-
-function RandomQuotes() {
-  const [quote, setQuote] = useState("");
-  
-  const generateQuote = () => {
-    let randomNumber = Math.floor(Math.random() * 4);
-    if (randomNumber === 0) {
-      setQuote("And as you gaze into the abyss, the abyss gazes into you...");
-    } else if (randomNumber === 1) {
-      setQuote("I think, therefore I am...");
-    } else if (randomNumber === 2) {
-      setQuote("The only thing to fear is fear itself...");
-    } else {
-      setQuote("Not a step back...");
-    }
-
-    return quote;
-  }
-  return(
-    <div>
-      <button onClick={generateQuote}>Generate</button>
-      <h1>{quote}</h1>
-    </div>
-  )
-
-}
-
 function Counter() {
   const [count, setCount] = useState(0);
 
-  const addNumber = () => {
-    setCount((prevCount) => prevCount + 1);
+  const addCount = () => {
+    setCount(count + 1);
   }
+
   return(
     <div>
-      <button onClick={addNumber}>CounterADD</button>
+      <button type='button' onClick={addCount}>Count ADD</button>
       <h1>{count}</h1>
     </div>
   )
+
 }
-
-
-function ToDoList() {
-  const [note, setNote] = useState("");
-
-  const addNote = () => {
-
-  }
-
-  return( 
-    <div>
-      <input type="text" />
-      <button type="button">Add</button>
-    </div>
-  )
-}
-
-
-
-function StopWatch() {
-  const [timer, setTimer] = useState(0);
-  const [isActive, setIsActive] = useState(true);
-
-    useEffect(() => {
-    let interval = setInterval(()=> {
-      setTimer(timer => timer + 1);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-  
-  return (
-    <div>
-      <h1>{timer}</h1>
-    </div>
-  )};
-
-
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <HandleInput />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
