64c5f34166807510dbf8fd88cb7cc4c0173b83d3
Daniel Acosta <danielacosta10@aol.com>
2022-03-24 21:57:38 -0700

    setinterval practice

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -4,53 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function DigitalClock() {
+  const [time, setTime] = useState(0);
 
-function GetInfo() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
 
-  // Note: the empty deps array [] means
-  // this useEffect will run once
-  // similar to componentDidMount()
-  useEffect(() => {
-    fetch("https://api.hatchways.io/assessment/students")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
+  useEffect(()=> {
+    let intervalId = setInterval(setTime(time + 1), 10);
+    
+  }, []);
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <ul>
-        {items.map(item => (
-          <li key={item.id}>
-            {item.name} {item.price}
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+  return(
+    <div><h1>{time}</h1></div>
+  )};
 
 ReactDOM.render(
   <React.StrictMode>
-  <GetInfo />
+  <DigitalClock />
   </React.StrictMode>,
   document.getElementById('root')
 );
