b0d9fab3d2bbf6cce363718505cd3d38ee9fb72b
Daniel Acosta <danielacosta10@aol.com>
2022-04-02 19:28:15 -0700

    MBTI test final result

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -9,57 +9,96 @@ function PersonalityTest() {
   const [extrovert, setExtrovert] = useState(0);
   const [sensing, setSensing] = useState(0);
   const [thinking, setThinking] = useState(0);
-  const [perceiving, setPerceiving] = useState(0);
+  const [judging, setJudging] = useState(0);
+
+  const addExtrovert = () => {
+    setExtrovert(extrovert + 1);
+  }
+
+  const minusExtrovert = () => {
+    setExtrovert(extrovert - 1);
+  }
+
+  const addSensing = () => {
+    setSensing(sensing + 1);
+  }
+
+  const minusSensing = () => {
+    setSensing(sensing - 1);
+  }
+
+  const addThinking = () => {
+    setThinking(thinking + 1);
+  }
+
+  const minusThinking = () => {
+    setThinking(thinking - 1);
+  }
+
+  const addJudging = () => {
+    setJudging(judging + 1);
+  }
+
+  const minusJudging = () => {
+    setJudging(judging - 1);
+  }
+
+
 
   return(<div>
 
     <h3>Do you get energized by social events or would you prefer to be alone?</h3>
-    <button type="button">Yes</button>
-    <button type="button">No</button>
+    <button type="button" onClick={addExtrovert}>Yes</button>
+    <button type="button" onClick={minusExtrovert}>No</button>
 
     <h3>Are you more of a talker than a listener?</h3>
-    <button type="button">Yes</button>
-    <button type="button">No</button>
+    <button type="button" onClick={addExtrovert}>Yes</button>
+    <button type="button" onClick={minusExtrovert}>No</button>
 
     <h3>Do you love attention?</h3>
-    <button type="button">Yes</button>
-    <button type="button">No</button>
+    <button type="button" onClick={addExtrovert}>Yes</button>
+    <button type="button" onClick={minusExtrovert}>No</button>
 
     <h3>Do you live more for the moment or focus on the future?</h3>
-    <button type="button">Yes</button>
-    <button type="button">No</button>
+    <button type="button" onClick={addSensing}>Yes</button>
+    <button type="button" onClick={minusSensing}>No</button>
 
     <h3>Do you prefer to play now and work later?</h3>
-    <button type="button">Yes</button>
-    <button type="button">No</button>
+    <button type="button" onClick={addSensing}>Yes</button>
+    <button type="button" onClick={minusSensing}>No</button>
 
     <h3>Do you like be spontaneous rather than have a planned schedule?</h3>
-    <button type="button">Yes</button>
-    <button type="button">No</button>
+    <button type="button" onClick={addSensing}>Yes</button>
+    <button type="button" onClick={minusSensing}>No</button>
 
     <h3>Do you lack empathy?</h3>
-    <button type="button">Yes</button>
-    <button type="button">No</button>
+    <button type="button" onClick={addThinking}>Yes</button>
+    <button type="button" onClick={minusThinking}>No</button>
 
     <h3>Do you follow your head more than your heart?</h3>
-    <button type="button">Yes</button>
-    <button type="button">No</button>
+    <button type="button" onClick={addThinking}>Yes</button>
+    <button type="button" onClick={minusThinking}>No</button>
 
     <h3>Do you prefer your kids to be smart than kind ?</h3>
-    <button type="button">Yes</button>
-    <button type="button">No</button>
+    <button type="button" onClick={addThinking}>Yes</button>
+    <button type="button" onClick={minusThinking}>No</button>
 
     <h3>Do you like having a set daily routine?</h3>
-    <button type="button">Yes</button>
-    <button type="button">No</button>
+    <button type="button" onClick={addJudging}>Yes</button>
+    <button type="button" onClick={minusJudging}>No</button>
 
     <h3>Are you able to focus on tasks for long periods of time?</h3>
-    <button type="button">Yes</button>
-    <button type="button">No</button>
+    <button type="button" onClick={addJudging}>Yes</button>
+    <button type="button" onClick={minusJudging}>No</button>
 
     <h3>Do you resist temptation easily?</h3>
-    <button type="button">Yes</button>
-    <button type="button">No</button>
+    <button type="button" onClick={addJudging}>Yes</button>
+    <button type="button" onClick={minusJudging}>No</button>
+
+    <h1>{extrovert > 0 ? "E" : "I"}</h1>
+    <h1>{sensing > 0 ? "S" : "N"}</h1>
+    <h1>{thinking > 0 ? "T" : "F"}</h1>
+    <h1>{judging > 0 ? "J" : "P"}</h1>
 
   </div>)
 };
