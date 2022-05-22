dbcedb54c589451eaa6347895973f8f755e6299e
Daniel Acosta <danielacosta10@aol.com>
2022-05-21 18:00:18 -0700

    Practicing for loops in react:

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -5,148 +5,40 @@ import './index.css';
 import reportWebVitals from './reportWebVitals'; 
 
 function CalendarApp() {
+
   const [numberDay, setNumberDay] = useState({});
   const [month, setMonth] = useState();
-  const [numberArray, setNumberArray] = useState();
+  const [numberArray, setNumberArray] = useState([]);
   const [dayOfWeek, setDayOfWeek] = useState('');
 
 
 
 for (let i = 0; i < 365; i++) {
-
+setNumberArray(numberArray => [...numberArray, i])
 }; 
+
+
 
 let may = {
 
 }
 
-};
-
-
-function CombinationTest() {
-  return(<div>
-  <EmailInput />
-  <PhoneInput />
-        </div>)
-};
-
-
-function EmailInput() {
-
-    const [email, setEmail] = useState('');
-    const [display, setDisplay] = useState('');
-    const [toggle, setToggle] = useState(true);
-  
-    let inputDisplay = () => {
-  
-      return(<div>
-        <input type='text' id="test" onChange={e => setEmail(e.target.value)} />
-        <br></br>
-        <button type="button" id="drill" onClick={addEmail}>Add Email</button>
-        </div>)};
-  
-    let emailDisplay = () => {
-  
-      return(<div>
-        <button type='button' id="third" onClick={removeEmail}>Delete</button>
-      </div>)};
-  
-  
-    const addEmail = () => {
-      setDisplay(email);
-      setToggle(false);
-    };
-  
-    const removeEmail = () => {
-      setToggle(true);
-      setDisplay('')
-    };
-  
-  
-  
-    return(
-    <div>
-    
-    <h1>
-      {toggle ? <div>
-        <input type='text' id="test" onChange={e => setEmail(e.target.value)} />
-        <br></br>
-        <button type="button" id="drill" onClick={addEmail}>Add Email</button>
-        </div> : <div>
-        <button type='button' id="third" onClick={removeEmail}>Delete Email</button>
-      </div>}
-    
-    </h1>
-    {display}
-    </div>
-    )
-  };
-  
-  
-  
-
-
-
-
-function PhoneInput() {
-
-  const [phone, setPhone] = useState('');
-  const [display, setDisplay] = useState('');
-  const [toggle, setToggle] = useState(true);
-
-  let inputDisplay = () => {
-
-    return(<div>
-      <input type='text' id="test" onChange={e => setPhone(e.target.value.toString())} />
-      <br></br>
-      <button type="button" id="drill" onClick={addNumber}>Add</button>
-      </div>)};
-
-  let phoneDisplay = () => {
-
-    return(<div>
-      <button type='button' id="third" onClick={removeNumber}>Delete</button>
-    </div>)};
-
-
-  const addNumber = () => {
-    setDisplay(phone);
-    let element = document.getElementById('test');
-    let drill = document.getElementById('drill');
-    setToggle(false);
-  };
-
-  const removeNumber = () => {
-    let element = document.getElementById('third');
-    setToggle(true);
-    setDisplay('');
-  };
-
-
-
-  return(
+return(
   <div>
-  
-  <h1>
-    {toggle ? <div>
-      <input type='text' id="test" onChange={e => setPhone(e.target.value.toString())} />
-      <br></br>
-      <button type="button" id="drill" onClick={addNumber}>Add</button>
-      </div> : <div>
-      <button type='button' id="third" onClick={removeNumber}>Delete</button>
-    </div>}
-  
-  </h1>
-  {display}
+    <h1>{numberArray}s</h1>
   </div>
-  )
+)
+
 };
+
+
+
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-  <CombinationTest />
+  <CalendarApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
