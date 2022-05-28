a98c4209bac2d34cda44c53ca6a335b76fd5e102
Daniel Acosta <danielacosta10@aol.com>
2022-05-27 23:20:18 -0700

    JS fundamentals

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -12,6 +12,13 @@ function CalendarApp() {
   const [numberArray, setNumberArray] = useState([]);
   const [dayOfWeek, setDayOfWeek] = useState('');
 
+let testArr = [];  
+
+for (let i = 1; i < 18; i++) {
+  testArr.push(i);
+}
+
+
 const daysMonth = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
 
 const stringDays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
@@ -20,7 +27,7 @@ var arr = new Array(100).fill().map((d,i)=>++i)
 
 let objectMay;
 
-let may = { 
+/*let may = { 
   '1': "Sunday",
   '2':
   '3':
@@ -53,10 +60,10 @@ let may = {
   '30':
   '31':
 };
-
+*/
 return(
   <div>
-    <h1>{revisedArr}s</h1>
+    <h1>{testArr}s</h1>
   </div>
 )
 
